a146b9d04f693b1777600ee6cbaabfbc9373158f
SamG <srgoodspeed@gmail.com>
2015-10-06 15:05:56 -0400

    Created Project Repo
    Created and populated repo with current goose island build

---
+++ Assets/PanelSlide.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PanelSlide : MonoBehaviour {
+
+	bool isMoving = false;
+	bool isShowing = false;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+
+	void triggerPanel(){
+		if (!isMoving && !isShowing) {
+			isMoving = true;
+		} else if (isMoving && !isShowing) {
+			
+		}
+	}
+
+
+}
---
+++ Assets/Scripts/SplashFade.cs
@@ -0,0 +1,44 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class SplashFade : MonoBehaviour {
+	public GoogleAnalyticsV3 googleAnalytics;
+
+	bool splashFade = false;
+	int timer = 0;
+	int splashDur = 100;
+
+	 Image splashImage;
+	// Use this for initialization
+	void Start () {
+		splashImage = gameObject.GetComponent<Image> ();
+		DontDestroyOnLoad (this);
+
+		googleAnalytics.StartSession ();
+		googleAnalytics.LogScreen ("Splash");
+		googleAnalytics.DispatchHits ();
+		//googleAnalytics.
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		print (timer);
+	if (!splashFade) {
+			if (timer < splashDur) {
+				timer ++;
+			} else {
+				splashFade = true;
+			}
+		} else {
+			//Fade Splash Screen
+			if(splashImage.color.a > .02f){
+			splashImage.color = new Color(splashImage.color.r, splashImage.color.g,splashImage.color.b,splashImage.color.a - .02f);
+			}
+			else{
+				splashImage.gameObject.SetActive(false);
+			}
+		}
+	}
+}
